7c6d3664882e62a0bdaec0767b594c9d3c135d04
hellseye <mayankdhanuka899@gmail.com>
2026-03-31 23:40:35 +0530

    made cleaned ui

--- client/script.js
+++ client/script.js
@@ -1,25 +1,29 @@
 const BASE_URL = "http://localhost:5000/api";
+const time = document.getElementById("time");
+const coordsLat = document.getElementById("coords-lat");
+const coordsLon = document.getElementById("coords-lon");
+const speed = document.getElementById("speed");
+const altitude = document.getElementById("altitude");
 
 
 const map = L.map("map", {
   center: [20, 0],
-  zoom: 2,
-  minZoom: 2,
+  zoom: 3,
+  minZoom: 2.5,
+  maxZoom: 5,
   maxBounds: [
-    [-90, -180],
+    [-180, -180],
     [90, 180]
   ],
   maxBoundsViscosity: 1.0, 
-  worldCopyJump: false 
+  worldCopyJump: false
 });
 
 L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
   attribution: "&copy; OpenStreetMap",
   subdomains: "abcd",
-  maxZoom: 19,
-  noWrap: true
+  noWrap: true,
 }).addTo(map);
-
 
 const issIcon = L.divIcon({
   className: "",
@@ -48,6 +52,13 @@ function updatePath(lat, lon) {
   trail.setLatLngs(path);
 }
 
+function updateVal(lat, lon, speed_val, altitude_val){
+  coordsLat.innerText = lat
+    coordsLon.innerText = lon
+    speed.innerText = speed_val
+    altitude.innerText = altitude_val
+}
+
 async function fetchISS() {
   try {
     const res = await fetch(`${BASE_URL}/iss`);
@@ -55,6 +66,8 @@ async function fetchISS() {
 
     const lat = parseFloat(data.latitude);
     const lon = parseFloat(data.longitude);
+    const speed_val = parseFloat(data.velocity);
+    const altitude_val = parseFloat(data.altitude);
 
     issMarker.setLatLng([lat, lon]);
 
@@ -65,11 +78,24 @@ async function fetchISS() {
       firstLoad = false;
     }
 
+    updateVal(lat, lon, speed_val, altitude_val)
+
+
     issMarker.bindPopup(`🛰️ ISS<br>Lat: ${lat}<br>Lon: ${lon}`);
   } catch (err) {
     console.error("Error fetching ISS:", err);
   }
 }
+
+function getUTCTime() {
+  const now = new Date();
+
+  return now.toUTCString();
+}
+
+setInterval(() => {
+  time.innerText = getUTCTime()
+}, 1000)
 
 
 setInterval(fetchISS, 5000);
